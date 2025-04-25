5786a63547bb08dee8bd738f6bb1cb1ef0adfa85
jnbovfavhk <154667940+jnbovfavhk@users.noreply.github.com>
2025-04-25 11:08:39 +0400

    Update task5.py

--- task5.py
+++ task5.py
@@ -35,4 +35,26 @@ def f(x):
     return 96 * (x[0] ** 2) + 191 * x[0] * x[1] + 96 * (x[1] ** 2) - 91 * x[0] - 12 * x[1] + 86
 
 
+def fastest_descent(f, x0, e=1e-3):
+    x = np.array(x0, dtype=float)
+    grad_f = grad(f)
+
+    for _ in range(10000):
+        gradient = grad_f(x)
+        grad_norm = np.linalg.norm(gradient)
+
+        # Проверка условия
+        if grad_norm < e:
+            break
+
+        alpha_optimized = golden_section_search(phi, a=0, b=1, e=1e-5)
+        
+        x = x - alpha_optimized * gradient
+
+        return x, f(x)
+
+
+
+
+
 print(gradient_descent(f, [0, 0]))
